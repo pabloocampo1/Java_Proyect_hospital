f49ef1efb1357a17de548d8d623d52cb789649ba
pablo ocampo <juanpabloocampoleon@gmail.com>
2024-07-15 11:46:13 -0500

    mas pruebas de ramas

--- Administrador.java
+++ Administrador.java
@@ -79,14 +79,14 @@ class Administrador extends Person {
 
 
     public void menuDoctor(Doctors user){
-        System.out.println("Bienvenido, "+ user.getName());;
+        System.out.println("Bienvenido, "+ user.getName());
         
     };
 
 
     public void menuAdmin (Administrador user) {
         while (true) {
-            System.out.println(" 1. Agregar doctor\n 2. Editar datos del doctor\n 3. Eliminar doctor\n 4. Buscar Doctor\n 5. Ver todos los doctores\n 6. Agregar un paciente\n 7.");
+            System.out.println(" 1. Agregar doctor\n 2. Editar datos del doctor\n 3. Eliminar doctor\n 4. Buscar Doctor\n 5. Ver todos los doctores\n 6. Agregar un paciente\n 7.ggf");
             break;
         }
     };
